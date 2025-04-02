c2ee573ee627c4b20d9934999777c1f19673a74c
Neil Vaytet <neil.vaytet@ess.eu>
2025-04-02 22:11:24 +0200

    cleanup

--- run/play.py
+++ run/play.py
@@ -4,17 +4,12 @@
 import tilthenightends as tne
 from survivor_bot import team
 
-# bots = []
-# for repo in glob.glob("*_bot"):
-#     module = importlib.import_module(f"{repo}")
-#     bots.append(module.Bot())
 
 tne.play(
     team=team,  # Must be a Team instance with players and a strategist
     world="forest",  # "forest", "mountain", "desert", "mine"
     music=False,
     seed=None,
-    # follow=True,
     xp_cheat=1.0,
     show_scenery=True,  # Set to False to hide the scenery for better performance
     speedup=1.0,  # Set to 2.0 for double speed
